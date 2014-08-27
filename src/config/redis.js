f0b45345ba10c18846cf5df1316898c882e0fb2d
f03lipe <pires.a.felipe@gmail.com>
2014-08-27 06:22:00 -0300

    Let's try this.

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,11 +2,17 @@
 var redis = require('redis')
 var url = require('url')
 
-if (process.env.REDISTOGO_URL) {
-	var redisUrl = url.parse(process.env.REDISTOGO_URL)
-	module.exports = redis.createClient(redisUrl.port, redisUrl.hostname, {
-		auth_pass: redisUrl.auth && redisUrl.auth.split(':')[1]
-	})
-} else {
-	module.exports = redis.createClient()
-}
+// if (global._redisClient) {
+// 	console.log('using old')
+// 	module.exports = global._redisClient;
+// } else {
+	console.log('new redis')
+	if (process.env.REDISTOGO_URL) {
+		var redisUrl = url.parse(process.env.REDISTOGO_URL)
+		module.exports = redis.createClient(redisUrl.port, redisUrl.hostname, {
+			auth_pass: redisUrl.auth && redisUrl.auth.split(':')[1]
+		})
+	} else {
+		module.exports = redis.createClient()
+	}
+// }
