485d1e149774d0343552b1c064d7acf9e35148ce
f03lipe <pires.a.felipe@gmail.com>
2014-09-15 16:20:45 -0300

    New favicon.

--- src/core/locals/all.js
+++ src/core/locals/all.js
@@ -7,64 +7,40 @@ var nconf = require('nconf')
 module.exports = function (app) {
 	var logger = app.get("logger");
 
-	var urls = { // cheating, while we can't proxy express.Router() calls
-		settings: '/settings',
-		faq: '/faq',
-		about: '/sobre',
-		twitter: 'http://twitter.com/qilabsorg',
-		facebook: 'http://facebook.com/qilabsorg',
-		logout: '/api/me/logout',
-		blog: 'http://blog.qilabs.org',
-	}
-
-	_.extend(app.locals, {
+	var obj = {
 		errors: {},
-		getPageUrl: function (name, args) { // (name, args... to fill pageurl if known)
-			// var urls = app.locals.urls;
-			if (typeof urls[name] !== 'undefined') {
-				/* Fill in arguments to url passed in arguments. */
-				var url = urls[name],
-					regex = /:[\w_]+/g;
-				/* This doesn't account for optional arguments! TODO */
-				if ((url.match(regex) || []).length !== _.size(args))
-					throw "Wrong number of keys to getPageUrl.";
+		// getUrl: function (name, args) { // (name, args... to fill pageurl if known)
+		// 	// var urls = app.locals.urls;
+		// 	if (typeof urls[name] !== 'undefined') {
+		// 		/* Fill in arguments to url passed in arguments. */
+		// 		var url = urls[name],
+		// 			regex = /:[\w_]+/g;
+		// 		/* This doesn't account for optional arguments! TODO */
+		// 		if ((url.match(regex) || []).length !== _.size(args))
+		// 			throw "Wrong number of keys to getPageUrl.";
 
-				if (args) {
-					var a = url.replace(regex, function (occ) {
-						var argName = occ.slice(1,occ.length);
-						if (!(argName in args))
-							throw "Invalid argument '"+argName+"' to url '"+url+"'' getPageUrl. ";
-						return args[occ.slice(1,occ.length)];
-					});
-					return a
-				} else {
-					return url;
-				}
-
-			} else {
-				if (app.get('env') !== 'production') {
-					console.trace();
-					throw "Page named '"+name+"' was referenced but doesn't exist.";
-				} else {
-					logger.error("Page named '"+name+"' was referenced but doesn't exist.");
-					console.trace();
-				}
-				return "#";
-			}
-		},
-		getQILogo: function () {
-			return "/static/images/logo.png";
-		},
-		postTypes: {
-			discussion: {
-				translated: 'Discussion',
-				iconClass: 'icon-question'
-			},
-			note: {
-				translated: 'Note',
-				iconClass: 'icon-bulb',
-			},
-		},
+		// 		if (args) {
+		// 			var a = url.replace(regex, function (occ) {
+		// 				var argName = occ.slice(1,occ.length);
+		// 				if (!(argName in args))
+		// 					throw "Invalid argument '"+argName+"' to url '"+url+"'' getPageUrl. ";
+		// 				return args[occ.slice(1,occ.length)];
+		// 			});
+		// 			return a
+		// 		} else {
+		// 			return url;
+		// 		}
+		// 	} else {
+		// 		if (app.get('env') !== 'production') {
+		// 			console.trace();
+		// 			throw "Page named '"+name+"' was referenced but doesn't exist.";
+		// 		} else {
+		// 			logger.error("Page named '"+name+"' was referenced but doesn't exist.");
+		// 			console.trace();
+		// 		}
+		// 		return "#";
+		// 	}
+		// },
 		pageMap: require('../pages.js').data,
 		assetUrl: function (mediaType) {
 			var relPath = pathLib.join.apply(null, arguments);
@@ -88,8 +64,17 @@ module.exports = function (app) {
 		app: {
 			env: nconf.get('env')
 		},
-	});
+		urls: { // while we can't proxy express.Router() calls and namefy them...
+			settings: '/settings',
+			faq: '/faq',
+			about: '/sobre',
+			twitter: 'http://twitter.com/qilabsorg',
+			facebook: 'http://facebook.com/qilabsorg',
+			logo: "/static/images/logo.png",
+			logout: '/api/me/logout',
+			blog: 'http://blog.qilabs.org',
+		},
+	};
 
-	app.locals.url = app.locals.getPageUrl;
-	app.locals.urls = urls;
+	_.extend(app.locals, obj);
 }
