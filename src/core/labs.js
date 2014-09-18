7bfbe336ebad2818dddc53959549a01285cf449e
f03lipe <pires.a.felipe@gmail.com>
2014-09-18 17:30:26 -0300

    Labs tags.

--- src/core/labs.js
+++ src/core/labs.js
@@ -20,22 +20,66 @@ module.exports = {
 			path: 'matematica',
 			icon: 'icon-pi-outline',
 			guidePath: '/guias/olimpiadas-matematica',
+			children: {
+				'problema': {
+					name: "Problema",
+				},
+				'experiencia': {
+					name: "Experiência",
+				},
+				'duvida': {
+					name: "Dúvida",
+				},
+			},
 		},
 		'physics': {
 			name: 'Física',
 			icon: 'icon-rocket2',
 			path: 'fisica',
+			children: {
+				'problema': {
+					name: "Problema",
+				},
+				'experiencia': {
+					name: "Experiência",
+				},
+				'duvida': {
+					name: "Dúvida",
+				},
+			},
 		},
 		'chemistry': {
 			name: 'Química',
 			path: 'quimica',
 			icon: 'icon-lab',
+			children: {
+				'problema': {
+					name: "Problema",
+				},
+				'experiencia': {
+					name: "Experiência",
+				},
+				'duvida': {
+					name: "Dúvida",
+				},
+			},
 		},
 		'programming': {
 			name: 'Programação',
 			path: 'programacao',
 			icon: 'icon-terminal',
 			guidePath: '/guias/programacao',
+			children: {
+				'problema': {
+					name: "Problema",
+				},
+				'experiencia': {
+					name: "Experiência",
+				},
+				'duvida': {
+					name: "Dúvida",
+				},
+			},
 		}, 
 		'meta': {
 			name: 'Qi Meta',
