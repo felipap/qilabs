063a3bb7a3e94732a50713ddecc30231b3bbbf0c
f03lipe <pires.a.felipe@gmail.com>
2014-11-04 06:11:36 -0200

    Theoretically, the tour.

--- assets/js/devbundle.js
+++ assets/js/devbundle.js
@@ -1688,20 +1688,41 @@ require('bootstrap-tour')
 window.tour = new Tour({
   steps: [
     {
-      title: "Title of my step",
-      content: "Content of my step",
+      title: "Bem vindo ao QI Labs!",
+      content: "A comunidade online para extra-curriculares. Nossa plataforma conecta jovens interessados nos mesmos assuntos.",
       placement: 'bottom',
       orphan: true,
       backdrop: true,
-    }
+    },
+    {
+      title: "Habemus <strong>Notificações</strong>",
+      content: "Aqui você pode ver as suas notificações.",
+      element: '#tour-nav-bell',
+      placement: 'bottom',
+      // backdrop: true,
+    },
+    {
+      title: "Habemus <strong>Pontos de Reputação</strong>",
+      content: "Aqui você pode ver a sua reputação. Você ganha pontos de reputação quando usuários votam nas suas publicações.",
+      element: '#tour-karma',
+      placement: 'bottom',
+      // backdrop: true,
+    },
+    {
+      // title: "Menu dos laboratórios",
+      content: "Nessa barra lateral você pode acessar nossas guias e laboratórios. Os laboratórios são grupos separados por assuntos.",
+      element: '#sidebar',
+      placement: 'right',
+      // backdrop: true,
+    },
   ],
   debug: true,
 });
 
 // tour.init();
-// setTimeout(function () {
-//   tour.restart();
-// }, 500)
+setTimeout(function () {
+  tour.restart();
+}, 500)
 
 module.exports = tour;
 },{"bootstrap-tour":31,"jquery":38,"underscore":50}],10:[function(require,module,exports){
