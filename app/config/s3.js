fdff7714e3f48f8441d6aabc53ab02d950336cc0
f03lipe <pires.a.felipe@gmail.com>
2015-03-01 18:33:27 -0300

    Requirejs → browserify

--- app/config/s3.js
+++ app/config/s3.js
@@ -11,18 +11,3 @@ module.exports = function (app) {
 	})
 	return client;
 }
-// var buffer = new Buffer('a string of data');
-// var headers = {
-// 	'x-amz-acl': 'public-read', // if you want anyone to be able to download the file
-// 	'Content-Type': 'text/plain',
-//   // 'Contnt-Length': buffer.length,
-// };
-
-// client.putBuffer(buffer, '/string.txt', headers, function(err, res){
-// 	if (err)
-// 		console.error(err);
-// 	console.log('status', res.statusCode)
-// 	res.on('data', function (chunk) {
-// 		console.log('BODY: ' + chunk);
-// 	});
-// });
